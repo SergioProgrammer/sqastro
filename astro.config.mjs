be9eb20dcf0b274c72738793414e7239fb9d9149
SergioProgrammer <sergiosanpacheco@gmail.com>
2025-01-19 21:47:21 +0000

    2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://github.com/SergioProgrammer/sqastro/",
-    base: "sqastro",
+    base: "/sqastro",
 });
